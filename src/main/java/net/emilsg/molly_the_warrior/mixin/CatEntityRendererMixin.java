8a0cc2b70e49bdc7a98a167f32315597d5702a63
EmilSG <emilsvardgronvikacc@gmail.com>
2025-03-10 20:46:46 +0100

    Initial Commit

--- src/main/java/net/emilsg/molly_the_warrior/mixin/CatEntityRendererMixin.java
+++ src/main/java/net/emilsg/molly_the_warrior/mixin/CatEntityRendererMixin.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(CatRenderer.class)
 public class CatEntityRendererMixin {
     @Unique
-    private static final ResourceLocation MOLLY_TEXTURE = new ResourceLocation(MollyTheWarrior.MODID, "textures/entity/cat/molly.png");
+    private static final ResourceLocation MOLLY_TEXTURE = ResourceLocation.fromNamespaceAndPath(MollyTheWarrior.MODID, "textures/entity/cat/molly.png");
 
     @Inject(
             method = "getTextureLocation(Lnet/minecraft/world/entity/animal/Cat;)Lnet/minecraft/resources/ResourceLocation;",
